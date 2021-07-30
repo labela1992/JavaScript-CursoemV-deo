bd69453cc746a9ad8c1f79914b948cd71b035fc6
Gustavo <gustavo.goncalves@solve4me.com.br>
2021-07-30 16:48:50 -0300

    Add ex008/9/10/11/12/13

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60.5
+console.log(`A velocidade do teu carro é ${velocidade}Km/h`)
+if (velocidade > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre utilizando o cinto de segurança')
